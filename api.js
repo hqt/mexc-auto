e0a6fe34d294409005885eefb95a6fd68f1d92d6
Huynh Quang Thao <huynhquangthao@gmail.com>
2022-06-09 12:33:43 +0800

    Update

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 const notifier = require('node-notifier');
 
 class Coin {
-  constructor(symbol, changed) {
+  constructor(symbol, price, changed) {
     this.symbol = symbol;
+    this.price = price;
     this.changed = changed;
   }
 }
@@ -16,8 +17,9 @@ function run() {
     let coins = [];
     for (let item of data) {
       let symbol = item['symbol'];
+      let price = item['lastPrice'];
       let rate = (item['riseFallRate']*100).toFixed(3);;
-      coins.push(new Coin(symbol, rate));
+      coins.push(new Coin(symbol, price, rate));
     }
     
     coins.sort(function(a, b){
@@ -28,7 +30,7 @@ function run() {
     console.log("----------------");
     console.log(coins.slice(coins.length-15, coins.length));
 
-    let msg = `${coins[0].symbol} increased ${coins[0].changed}%`;
+    let msg = `${coins[0].symbol} increased ${coins[0].changed}% - price: ${coins[0].price}`;
     notifier.notify(
       {
       title: 'Price Alert',
